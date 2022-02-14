f4983a1f09a936f3961d96115d59634e3cc378de
folkert van heusden <mail@vanheusden.com>
2022-02-14 21:53:14 +0100

    types

--- ipfix.h
+++ ipfix.h
@@ -1,5 +1,6 @@
 #include <map>
 #include <stdint.h>
+#include <string>
 #include <vector>
 
 
@@ -11,10 +12,39 @@ typedef struct
 	uint32_t enterprise_number;
 } information_element_t;
 
+typedef enum {
+	dt_reserved = -1,
+	dt_octetArray = 0,
+	dt_unsigned8,
+	dt_unsigned16,
+	dt_unsigned32,
+	dt_unsigned64,
+	dt_signed8,
+	dt_signed16,
+	dt_signed32,
+	dt_signed64,
+	dt_float32,
+	dt_float64,
+	dt_boolean,
+	dt_macAddress,
+	dt_string,
+	dt_dateTimeSeconds,
+	dt_dateTimeMilliseconds,
+	dt_dateTimeMicroseconds,
+	dt_dateTimeNanoseconds,
+	dt_ipv4Address,
+	dt_ipv6Address,
+	dt_basicList,
+	dt_subTemplateList,
+	dt_subTemplateMultiList
+} data_type_t;
+
 class ipfix
 {
 private:
 	std::map<uint16_t, std::vector<information_element_t> > templates;
+
+	std::map<uint16_t, std::pair<std::string, data_type_t> > field_types;
 
 public:
 	ipfix();
